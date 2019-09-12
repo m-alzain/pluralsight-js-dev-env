cbf058e36e1a44bf3a4eed2ed8701d2639ad6202
Mohamed Alzain <m.alzain248@outlook.com>
2019-09-12 16:59:02 +0300

    a lot of trouble because of webpack express integration and babel 7 configuration; webpack.config.dev.js, package.json , srcServer.js and .babelrc have been changed.

---
+++ webpack.config.dev.js
@@ -0,0 +1,64 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+export default {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'src'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+
+  plugins: [
+    // Create HTML file that includes reference to bundled JS.
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: true
+    })
+  ],
+
+  module: {
+    rules: [
+      // { test: /\.css$/, use: 'css-loader' },
+      // { test: /\.ts$/, use: 'ts-loader' },
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader'
+      }
+    ]
+  }
+};
+
+// export default {
+//   // debug: true, // WebpackOptionsValidationError: Invalid configuration object.
+//   devtool: 'inline-source-map',
+//   // noInfo: false, //WebpackOptionsValidationError: Invalid configuration object.
+//   entry: [
+//     path.resolve(__dirname, 'src/index')
+//   ],
+//   target: 'web',
+//   output: {
+//     path: path.resolve(__dirname, 'src'),
+//     publicPath: '/',
+//     filename: 'bundle.js'
+//   },
+//   plugins: [
+//     // Create HTML file that includes reference to bundled JS.
+//     new HtmlWebpackPlugin({
+//       template: 'src/index.html',
+//       inject: true
+//     })
+//   ],
+
+//   module: {
+//     rules: [
+//       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+//       {test: /\.css$/, loaders: ['style','css']}
+//     ]
+//   }
+// }
